811802c2b1af01ea87c7eb901addc5dc53df6a3c
UserCoder12345 <142465052+UserCoder12345@users.noreply.github.com>
2025-04-19 02:07:45 -0300

    New clients can now see other stickmans when connecting
    this took far too long to debug

--- script.js
+++ script.js
@@ -5,12 +5,13 @@ chat.style.fontFamily = "Arial, Times";
 chat.style.backgroundColor = "rgba(0,0,255,0.3)";
 chat.style.color = "#FFF";
 chat.style.textShadow = "0px 0px 5px black";
-var stick_xy = [512,-1,0,5];
-// var stick_id = Math.floor(Math.random() * 9999).toString().padStart(4,"0"); // Not used yet
+const start_pos = [512,-1,0,5];
+var stick_xy = start_pos;
+var stick_id = Math.floor(Math.random() * 9999).toString().padStart(4,"0"); // TODO: id collision check?
 
-/* Events:
-	!spos charX tileX - Draws stickman at said X.
-  !sb4 charX tileX - Erases stickman at said X, always sent before !spos
+/* Events (asterisk means the value is optional):
+	!spos charX tileX stickid* - Draws stickman at said X. If stickid is included, the event will be ignored by every client except those who have the matching stick_id.
+  !sb4 charX tileX - Erases stickman at said X, sent before !spos requests (unless the requests have a stickid)
   !snew - Adds a new stickman to stick_xy and sends a client message, sent only when the script is run.
 */
 
@@ -18,9 +19,9 @@ var stick_xy = [512,-1,0,5];
 document.addEventListener("keyup", function(e) {
 	switch (e.key) {
     case "ArrowLeft":
-			network.cmd(`!sb4${stick_xy[0]}X${stick_xy[2]}`);		
+			network.cmd(`!sb4${stick_xy[0]}X${stick_xy[2]}`);	// Erase previous stickman...
 			stick_xy = coordinateAdd(...stick_xy,0,0,-1,0);
-			network.cmd(`!spos${stick_xy[0]}X${stick_xy[2]}`);		
+			network.cmd(`!spos${stick_xy[0]}X${stick_xy[2]}`); // ...and draw the next one
 			break;
     case "ArrowRight":
 			network.cmd(`!sb4${stick_xy[0]}X${stick_xy[2]}`);		
@@ -74,6 +75,7 @@ function write_text(str,col,loc) {
 }
 
 // Client
+// TODO: detect client disconnect
 var draw_tx = 0; // tileX
 var draw_cx = 0; // charX
 var tx_cx = [];
@@ -84,15 +86,22 @@ var erase_tx_cx = 0;
 
 var draw_pos = [];
 var erase_pos = [];
+var sender_stickid = null;
 w.on("cmd",e=>{
 	switch (true) {
    case e.data.startsWith("!spos"):
 		tx_cx = e.data.slice(5);
-		tx_cx = tx_cx.split("X");
+		tx_cx = tx_cx.split("X"); // Get tileX and charX (and stick_id if included) from the message
 		draw_cx = Number(tx_cx[0]);
 		draw_tx = Number(tx_cx[1]);
 		draw_pos = [draw_cx,-1,draw_tx,5];
-		stickman(draw_pos);
+		if(tx_cx[2]) { // If this is a targeted request, then...
+			if(tx_cx[2] == stick_id) { // ...only the targeted client will process this request
+				stickman(draw_pos);
+			}
+		} else { // All clients will process this request
+			stickman(draw_pos);
+    }
 		break;
    case e.data.startsWith("!sb4"):
 		erase_tx_cx = e.data.slice(4);
@@ -103,9 +112,16 @@ w.on("cmd",e=>{
 		stick_erase(erase_pos);
 		break;
    case e.data.startsWith("!snew"):
-		stickman([512,-1,0,5]);
-		clientChatResponse("Stickman connected"); // TODO: show username/id instead of "Stickman"
+		sender_stickid = e.data.slice(5);
+		stickman(start_pos);
+		if(sender_stickid !== stick_id){
+			clientChatResponse("Stickman connected"); // TODO: show username/id instead of "Stickman"
+			network.cmd(`!spos${stick_xy[0]}X${stick_xy[2]}X${sender_stickid}`); // Send stickman position so the new client can see everyone
+		} else {
+			clientChatResponse("Connected!");
+		}
   };
 });
 
-network.cmd("!snew");
+network.cmd(`!snew${stick_id}`);
+// w.on("cmd",e=>{console.table(e)}); // use for debugging
