ead29d3ba534a3e87045fe4fe19058e7e01fd86d
UserCoder12345 <142465052+UserCoder12345@users.noreply.github.com>
2025-04-20 00:39:43 -0300

    Update script.js
    Stickmans can now change their color.

--- script.js
+++ script.js
@@ -5,13 +5,15 @@ chat.style.fontFamily = "Arial, Times";
 chat.style.backgroundColor = "rgba(0,0,255,0.3)";
 chat.style.color = "#FFF";
 chat.style.textShadow = "0px 0px 5px black";
-const start_pos = [512,-1,0,5];
+const start_pos = [512,-1,0,5]; // change if needed
 var stick_xy = start_pos;
 var stick_id = Math.floor(Math.random() * 9999).toString().padStart(4,"0"); // TODO: id collision check?
+var stick_color = 0x000000;
+w.doGoToCoord(0,128); // change if needed
 
 /* Events (asterisk means the value is optional):
-	!spos charX tileX stickid* - Draws stickman at said X. If stickid is included, the event will be ignored by every client except those who have the matching stick_id.
-  !sb4 charX tileX - Erases stickman at said X, sent before !spos requests (unless the requests have a stickid)
+	!spos charX tileX stickid* color* - Draws stickman at said X. If stickid is included, the event will be ignored by every client except those who have the matching stick_id. If color is included, the stickman will be drawn in said color.
+  !sb4 charX tileX - Erases stickman at said X, sent before !spos requests without a stickid
   !snew - Adds a new stickman to stick_xy and sends a client message, sent only when the script is run.
 */
 
@@ -19,33 +21,54 @@ var stick_id = Math.floor(Math.random() * 9999).toString().padStart(4,"0"); // T
 document.addEventListener("keyup", function(e) {
 	switch (e.key) {
     case "ArrowLeft":
-			network.cmd(`!sb4${stick_xy[0]}X${stick_xy[2]}`);	// Erase previous stickman...
+			if(stick_color !== 0x000000) {
+			network.cmd(`!sb4${stick_xy[0]}X${stick_xy[2]}XC${stick_color}`);	// Erase previous stickman...
 			stick_xy = coordinateAdd(...stick_xy,0,0,-1,0);
-			network.cmd(`!spos${stick_xy[0]}X${stick_xy[2]}`); // ...and draw the next one
+			network.cmd(`!spos${stick_xy[0]}X${stick_xy[2]}XC${stick_color}`); // ...and draw the next one
+      } else { // if col isn't defined it'll be 0 anyway so no need to include it
+			network.cmd(`!sb4${stick_xy[0]}X${stick_xy[2]}`);
+			stick_xy = coordinateAdd(...stick_xy,0,0,-1,0);
+			network.cmd(`!spos${stick_xy[0]}X${stick_xy[2]}`);
+			}
 			break;
     case "ArrowRight":
-			network.cmd(`!sb4${stick_xy[0]}X${stick_xy[2]}`);		
+			if(stick_color !== 0x000000) {
+			network.cmd(`!sb4${stick_xy[0]}X${stick_xy[2]}XC${stick_color}`);	// Erase previous stickman...
 			stick_xy = coordinateAdd(...stick_xy,0,0,1,0);
-			network.cmd(`!spos${stick_xy[0]}X${stick_xy[2]}`);		
+			network.cmd(`!spos${stick_xy[0]}X${stick_xy[2]}XC${stick_color}`); // ...and draw the next one
+      } else { // if col isn't defined it'll be 0 anyway so no need to include it
+			network.cmd(`!sb4${stick_xy[0]}X${stick_xy[2]}`);
+			stick_xy = coordinateAdd(...stick_xy,0,0,1,0);
+			network.cmd(`!spos${stick_xy[0]}X${stick_xy[2]}`);
+			}
 		  break;
 	};
 });
 
 // Functions
+var regex = /^0x[0-9A-Fa-f]{6}$/;
+function hexstr_to_num(str) {
+	if(str.match(regex) === null) { // Prevent execution of bad code with regex
+		return 0x000000;
+	} else {
+		return eval(str); // yes I know, eval sucks
+	}
+}
+
 function stickman(pos,col){
-if(!col) { col = 0x000000 };
+if(!col) { col = 0x000000 } else { col = hexstr_to_num(col) };
 loc = pos;
-writeCharTo("O",0x000000,...loc,true,true,null,false,false,false,false,true);// Head
+writeCharTo("O",col,...loc,true,true,null,false,false,false,false,true);// Head
 loc = coordinateAdd(...loc,0,0,-1,1);
-writeCharTo("/",0x000000,...loc,true,true,null,false,false,false,false,true);// Left arm
+writeCharTo("/",col,...loc,true,true,null,false,false,false,false,true);// Left arm
 loc = coordinateAdd(...loc,0,0,1,0);
-writeCharTo("|",0x000000,...loc,true,true,null,false,false,false,false,true);// Body
+writeCharTo("|",col,...loc,true,true,null,false,false,false,false,true);// Body
 loc = coordinateAdd(...loc,0,0,1,0);
-writeCharTo('\\',0x000000,...loc,true,true,null,false,false,false,false,true);//Right arm
+writeCharTo('\\',col,...loc,true,true,null,false,false,false,false,true);//Right arm
 loc = coordinateAdd(...loc,0,0,-2,1);
-writeCharTo("/",0x000000,...loc,true,true,null,false,false,false,false,true);//Left leg
+writeCharTo("/",col,...loc,true,true,null,false,false,false,false,true);//Left leg
 loc = coordinateAdd(...loc,0,0,2,0);
-writeCharTo('\\',0x000000,...loc,true,true,null,false,false,false,false,true);//Right leg
+writeCharTo('\\',col,...loc,true,true,null,false,false,false,false,true);//Right leg
 };
 
 function stick_erase(pos){
@@ -87,6 +110,11 @@ var erase_tx_cx = 0;
 var draw_pos = [];
 var erase_pos = [];
 var sender_stickid = null;
+
+menu.addOption("Change Color",()=>{
+stick_color = prompt("Pick a color for your stickman.\nMust be a hex value and begin with 0x, like this:\n0x00FF00 (example)");
+network.cmd(`!spos${stick_xy[0]}X${stick_xy[2]}XC${stick_color}`);
+});
 w.on("cmd",e=>{
 	switch (true) {
    case e.data.startsWith("!spos"):
@@ -95,10 +123,18 @@ w.on("cmd",e=>{
 		draw_cx = Number(tx_cx[0]);
 		draw_tx = Number(tx_cx[1]);
 		draw_pos = [draw_cx,-1,draw_tx,5];
-		if(tx_cx[2]) { // If this is a targeted request, then...
-			if(tx_cx[2] == stick_id) { // ...only the targeted client will process this request
+		if(tx_cx[2]) { // Includes stickman color
+			if(tx_cx[2].startsWith("C")) { // 
+				stickman(draw_pos,tx_cx[2].slice(1)); // remove C from value
+			} else { // Is a targeted request
+			if(tx_cx[2] == stick_id) { // Only the targeted client will process this request
+				if(tx_cx[3]) { // Includes stickman color
+				stickman(draw_pos,tx_cx[3].slice(1));
+        } else {
 				stickman(draw_pos);
+				}
 			}
+      }
 		} else { // All clients will process this request
 			stickman(draw_pos);
     }
@@ -115,13 +151,21 @@ w.on("cmd",e=>{
 		sender_stickid = e.data.slice(5);
 		stickman(start_pos);
 		if(sender_stickid !== stick_id){
-			clientChatResponse("Stickman connected"); // TODO: show username/id instead of "Stickman"
-			network.cmd(`!spos${stick_xy[0]}X${stick_xy[2]}X${sender_stickid}`); // Send stickman position so the new client can see everyone
+			clientChatResponse("Stickman "+sender_stickid+" connected."); 
+			if(stick_color !== 0x000000) {
+			network.cmd(`!spos${stick_xy[0]}X${stick_xy[2]}X${sender_stickid}XC${stick_color}`); // Send stickman position so the new client can see everyone
+      } else {
+			network.cmd(`!spos${stick_xy[0]}X${stick_xy[2]}X${sender_stickid}`);
+			}
 		} else {
-			clientChatResponse("Connected!");
+			clientChatResponse("Connected! Your ID: "+stick_id);
 		}
-  };
+  }
 });
 
+// Wait for tiles to load first
+clientChatResponse("Connecting...")
+setTimeout(()=>{
 network.cmd(`!snew${stick_id}`);
+},2000);
 // w.on("cmd",e=>{console.table(e)}); // use for debugging
